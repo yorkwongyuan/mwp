29fde90211f489f3e273b12348b2626b66b06d62
yuan_wy_wang <631824375@qq.com>
2021-08-19 22:10:25 +0800

    修改文件

--- packages/@mwp/mwp-http/lib/mwp-http.js
+++ packages/@mwp/mwp-http/lib/mwp-http.js
@@ -4,4 +4,8 @@ module.exports = mwpHttp;
 
 function mwpHttp() {
     // TODO
+    this.getName = function (params) {
+        console.log('this.getName -> params', params)
+        return params
+    }
 }
---
+++ packages/@mwp/mwp-http/mwp-http.d.ts
@@ -0,0 +1,3 @@
+declare class mwpHttp {
+  getName(): string
+}
