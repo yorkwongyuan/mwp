ecfb16e24045c1d53680d403aae31187c776a8a7
yuan_wy_wang <631824375@qq.com>
2021-08-20 18:22:09 +0800

    he

--- packages/@kd-mwp/mwp-const/lib/mwp-const.js
+++ packages/@kd-mwp/mwp-const/lib/mwp-const.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// const mwpConst = {
-//     // REFERRAL: {
-//     //     UNRECEIVED: 'UNRECEIVED',
-//     //     HADACCEPTED: 'HADACCEPTED',
-//     //     HADREPORT: 'HADREPORT',
-//     //     CANCEL: 'CANCEL',
-//     //     NOTACCEPTED: 'NOTACCEPTED',
-//     //     REVOKE: 'REVOKE'
-//     // }
-//     name: 'york'
-// }
-
 module.exports = function getName (name) {
     this.name = name
     this.age = 12
