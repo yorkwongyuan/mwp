88a5895b868337aa528b5354310a6aa465cf638a
yuan_wy_wang <631824375@qq.com>
2021-08-24 23:20:31 +0800

    增加注释

--- packages/@kd-mwp/mwp-config/src/index.ts
+++ packages/@kd-mwp/mwp-config/src/index.ts
@@ -8,8 +8,11 @@ interface Iconfig {
 
 export const config:Iconfig = {
   baseUrl: {
+    /** 预发布环境地址 */
     preproduction: 'preproduction.mhealth100.com',
+    /** 正式环境地址 */
     www: 'www.mhealth100.com',
+    /** 开发环境地址 */
     dev: 'development.mhealth100.com'
   }
 }
