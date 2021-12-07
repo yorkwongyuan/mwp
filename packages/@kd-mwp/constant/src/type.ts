07f563cc00a1211dde806e6cdcde9f6438116c58
yorkwongyuan <631824375@qq.com>
2021-12-07 15:15:44 +0800

    代码提交增加注释

--- packages/@kd-mwp/constant/src/type.ts
+++ packages/@kd-mwp/constant/src/type.ts
@@ -2,14 +2,23 @@ import Vue from 'vue'
 // s001是平台管理员，js002是业务（机构）管理员
 // 枚举键
 enum enumKeys {
+  /** 平台管理员 */
   ADMIN = 'ADMIN',
+  /** 业务管理员 */
   BIZ_ADMIN = 'BIZ_ADMIN',
+  /** 志愿者 */
   VOLUNTEER = 'VOLUNTEER',
+  /** 医生 */
   DOCTOR = 'DOCTOR',
+  /** 护士 */
   NURSE = 'NURSE',
+  /** 药剂师 */
   DRUGGIST = 'DRUGGIST',
+  /** 影像师 */
   PHOTOER = 'PHOTOER',
+  /** 检验师 */
   CHECKER = 'CHECKER',
+  /** 健康从业者 */
   HEALTHER = 'HEALTHER'
 }
 type keys = Record<enumKeys, string>
