83455646c291de9dbdf9e08140161e3652e5f9bd
yorkwongyuan <631824375@qq.com>
2021-12-07 17:38:34 +0800

    编译

--- packages/@kd-mwp/constant/lib/type.d.ts
+++ packages/@kd-mwp/constant/lib/type.d.ts
@@ -1,21 +1,19 @@
 import Vue from 'vue';
-declare enum enumKeys {
-    ADMIN = "ADMIN",
-    BIZ_ADMIN = "BIZ_ADMIN",
-    VOLUNTEER = "VOLUNTEER",
-    DOCTOR = "DOCTOR",
-    NURSE = "NURSE",
-    DRUGGIST = "DRUGGIST",
-    PHOTOER = "PHOTOER",
-    CHECKER = "CHECKER",
-    HEALTHER = "HEALTHER"
-}
-declare type keys = Record<enumKeys, string>;
+export declare type enumkeys = 'ADMIN' | 'BIZ_ADMIN' | 'VOLUNTEER' | 'DOCTOR' | 'NURSE' | 'DRUGGIST' | 'PHOTOER' | 'CHECKER' | 'HEALTHER';
+export declare type enumValues = 'js001' | 'js002' | 'js003' | 'js004' | 'js005' | 'js006' | 'js007' | 'js008' | 'js009';
+declare type TroleConstant = {
+    [key in enumkeys]: string;
+};
+declare type TroleText = {
+    [key in enumValues]: string;
+};
 export interface Iconstant {
     /** vue注册插件 */
     install?: (vue: typeof Vue) => void;
     roles: {
-        roleConstant: keys;
+        roleConstant: TroleConstant;
+        roleText: TroleText;
+        doctorRoles: enumValues[];
     };
 }
 export {};
